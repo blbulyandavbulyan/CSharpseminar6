080cadb17c1eae5c7da6f90229a62b0f024d45b9
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-13 22:12:53 +0300

    task40, task44 solved

---
+++ task40/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+ */
+int getIntFromKeyboard(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool checkExists(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < b + a;
+}
+
+int a = getIntFromKeyboard("Введите сторону a: ");
+int b = getIntFromKeyboard("Введите сторону b: ");
+int c = getIntFromKeyboard("Введите сторону c: ");
+Console.WriteLine(checkExists(a, b, c) ? "Существует" : "Не существует");
+//if (checkExists(a, b, c))
+//{
+//    Console.WriteLine("Существует");
+//}
+//else Console.WriteLine("Не существует");
---
+++ task44/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+ */
+long[] getFibonaci(int count)
+{
+    long[] result = new long[count];
+    result[0] = 0;
+    result[1] = 1;
+    for(int i = 2; i < result.Length; i++)
+    {
+        result[i] = result[i - 1] + result[i - 2];//f(n) = f(n-1) + f(n-2)
+    }
+    return result;
+}
+Console.Write("Введите количество чисел фибоначи: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(string.Join(' ', getFibonaci(n)));
