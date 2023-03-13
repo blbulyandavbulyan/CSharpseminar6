3363570167883259918774f187c2c2f60e5ece50
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-03-13 20:28:03 +0300

    Добавьте файлы проекта.

---
+++ task39_demo/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу которая разворачивает массив
+void reverseArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length / 2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+int[] generateRandomArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    for(int i = 0; i <  length; i++)
+    {
+        arr[i] = Random.Shared.Next(min, max + 1);
+    }
+    return arr;
+}
+int[] arr = generateRandomArray(20, -200, 200);
+Console.WriteLine(string.Join(" ", arr));
+reverseArray(arr);
+Console.WriteLine(string.Join(" ", arr));
